69471785ea8a683b2dda19ea664618c01cc0ced9
Vickouma77 <Vickprestone20@gmail.com>
2025-02-27 17:59:09 +0300

    feat: implement configuration management, routing, and subscription handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
-use zero2prod::run;
+use std::net::TcpListener;
+use zero2prod::configuration::get_configurations;
+use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = std::net::TcpListener::bind("127.0.0.1:8000")?;
+    // Read the configuration file
+    let configuration = get_configurations().expect("Failed to read configurations");
+
+    // Bind the address
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    // Bind the listener
+    let listener = TcpListener::bind(address)?;
+
+    // Run the server
     run(listener)?.await
 }
