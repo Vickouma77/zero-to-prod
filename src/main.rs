0a140012fe4a6468380990712e6165f1d8cff4d9
Vickouma77 <Vickprestone20@gmail.com>
2025-03-05 10:25:39 +0300

    feat: update dependencies and enhance telemetry logging

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configurations;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // Initialize the logger
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
 
     // Read the configuration file
     let configuration = get_configurations().expect("Failed to read configurations");
