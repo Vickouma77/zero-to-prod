9fb5db343bbbaf8e1a32d95fe626b65ed635ed1e
Vickouma77 <Vickprestone20@gmail.com>
2025-04-03 17:42:01 +0300

    feat: add quickcheck support for SubscriberEmail tests with valid email generation

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -35,6 +35,8 @@ mod tests {
     use claim::assert_err;
     use fake::Fake;
     use fake::faker::internet::en::SafeEmail;
+    use fake::rand::SeedableRng;
+    use fake::rand::rngs::StdRng;
 
     #[test]
     fn empty_string_is_rejected() {
@@ -58,5 +60,22 @@ mod tests {
     fn valid_email_are_parsed_successfully() {
         let email = SafeEmail().fake();
         claim::assert_ok!(SubscriberEmail::parse(email));
+    }
+
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+            let mut rng = StdRng::seed_from_u64(u64::arbitrary(g));
+            let email = SafeEmail().fake_with_rng(&mut rng);
+
+            Self(email)
+        }
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        SubscriberEmail::parse(valid_email.0).is_ok()
     }
 }
