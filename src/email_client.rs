cba6ddef75033dfc0af93384132c9fceb131d9f6
Vickouma77 <Vickprestone20@gmail.com>
2025-04-12 22:04:08 +0300

    test: add timeout test for send_email method with extended server delay

--- src/email_client.rs
+++ src/email_client.rs
@@ -154,24 +154,20 @@ mod tests {
         let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
         let subject: String = Sentence(1..2).fake();
         let content: String = Paragraph(1..10).fake();
-
-        let response = ResponseTemplate::new(200).set_delay(std::time::Duration::from_secs(180));
-
+        let response = ResponseTemplate::new(200)
+            .set_delay(std::time::Duration::from_secs(180));
         Mock::given(any())
             .respond_with(response)
             .expect(1)
             .mount(&mock_server)
             .await;
-
         // Act
         let outcome = email_client
             .send_email(subscriber_email, &subject, &content, &content)
             .await;
-
         // Assert
         assert_err!(outcome);
     }
-
     #[tokio::test]
     async fn send_email_sends_the_expected_request() {
         // Arrange
