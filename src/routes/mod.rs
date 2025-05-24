4ee951a7489961d031f353129d3b061f05841b56
Vickouma77 <Vickprestone20@gmail.com>
2025-05-24 23:14:11 +0300

    feat: add publish_newsletter route and corresponding handler

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,9 +1,11 @@
 // Desc: Module for all routes
 
 mod health_check;
+mod newsletters;
 mod subscriptions;
 mod subscriptions_confirm;
 
 pub use health_check::*;
+pub use newsletters::*;
 pub use subscriptions::*;
 pub use subscriptions_confirm::*;
