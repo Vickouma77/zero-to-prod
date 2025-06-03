bb3c6ed36718f0a1292656acaf07c3b3ba9b8113
Vickouma77 <Vickprestone20@gmail.com>
2025-06-03 17:34:34 +0300

    feat: implement user authentication with credentials validation in publish_newsletter

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -6,7 +6,7 @@ use actix_web::http::{StatusCode, header};
 use actix_web::{HttpRequest, HttpResponse, ResponseError, web};
 use anyhow::Context;
 use base64::{Engine as _, engine::general_purpose};
-use secrecy::Secret;
+use secrecy::{Secret, ExposeSecret};
 use sqlx::PgPool;
 
 #[derive(serde::Deserialize)]
@@ -90,7 +90,16 @@ pub async fn publish_newsletter(
     email_client: web::Data<EmailClient>,
     request: HttpRequest,
 ) -> Result<HttpResponse, PublishError> {
-    let _credentials = basic_authentication(request.headers()).map_err(PublishError::AuthError)?;
+    let credentials = basic_authentication(request.headers()).map_err(PublishError::AuthError)?;
+
+    tracing::Span::current().record(
+        "username",
+        &tracing::field::display(&credentials.username),
+    );
+
+    let user_id = validate_credentials(credentials, &pool)
+        .await?;
+    tracing::Span::current().record("user_id", &tracing::field::display(user_id));
 
     let subscribers = get_confirmed_subscribers(&pool).await?;
 
@@ -125,6 +134,30 @@ pub async fn publish_newsletter(
 struct Credentials {
     username: String,
     password: Secret<String>,
+}
+
+async fn validate_credentials(
+    credentials: Credentials,
+    pool: &PgPool,
+) -> Result<uuid::Uuid, PublishError> {
+    let user_id: Option<_> = sqlx::query!(
+        r#"
+        SELECT user_id
+        FROM users
+        WHERE username = $1 AND password = crypt($2, password)
+        "#,
+        credentials.username,
+        credentials.password.expose_secret()
+    )
+    .fetch_optional(pool)
+    .await
+    .context("Failed to perform a query to validate auth credentials.")
+    .map_err(PublishError::UnexpectedError)?;
+
+    user_id
+        .map(|row| row.user_id)
+        .ok_or_else(|| anyhow::anyhow!("Invalid username or password."))
+        .map_err(PublishError::AuthError)
 }
 
 fn basic_authentication(headers: &HeaderMap) -> Result<Credentials, anyhow::Error> {
